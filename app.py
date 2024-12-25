10408884b958ed57f0b0a2f5ebd8ba4939d5da61
SanketChopade777 <142431102+SanketChopade777@users.noreply.github.com>
2024-12-25 09:32:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 import numpy as np
+import pandas
+import sklearn
 import pickle
 
-app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+sc = pickle.load(open('standscaler.pkl','rb'))
+mx = pickle.load(open('minmaxscaler.pkl','rb'))
 
-# Load models and scalers
-model = pickle.load(open('model.pkl', 'rb'))
-sc = pickle.load(open('standscaler.pkl', 'rb'))
-mx = pickle.load(open('minmaxscaler.pkl', 'rb'))
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predict():
-    # Extract input data
     N = request.form['Nitrogen']
     P = request.form['Phosporus']
     K = request.form['Potassium']
@@ -27,12 +28,10 @@ def predict():
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
     single_pred = np.array(feature_list).reshape(1, -1)
 
-    # Normalize and scale
     mx_features = mx.transform(single_pred)
     sc_mx_features = sc.transform(mx_features)
     prediction = model.predict(sc_mx_features)
 
-    # Crop dictionary
     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
                  14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
@@ -40,11 +39,11 @@ def predict():
 
     if prediction[0] in crop_dict:
         crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there.".format(crop)
+        result = "{} is the best crop to be cultivated right there".format(crop)
     else:
         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    
-    return render_template('index.html', result=result)
+    return render_template('index.html',result = result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
